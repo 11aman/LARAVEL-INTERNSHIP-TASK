c62c0d033713da1c1f281c47c317de6513deb4e3
sanju-shivam <sanjuofficial1999@gmail.com>
2020-09-18 13:30:04 +0530

    INTERNSHIP TASK 2

---
+++ database/migrations/2020_09_18_065236_create_blog_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlogTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blog', function (Blueprint $table) {
+            $table->id('BlogId');
+            $table->string('Name');
+            $table->integer('BlogcategoryId')->unsigned();
+            $table->foreign('BlogcategoryId')->references('CategoryId')->on('blogcategory');
+            $table->string('Bannerimage');
+            $table->string('Mainimage');
+            $table->string('Description');
+            $table->string('Status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blog');
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::any('allData','ProductController@allData');
+
+Route::post('add','ProductController@add');
+
+Route::any('Welcome','HomeController@Welcome');
+
+Route::post('addhome','HomeController@Welcomeadd');
+
+// Route::any('Media','HomeController@Media');
+
+// Route::any('Visit','HomeController@Visit');
+
+Route::any('Footer','HomeController@Footer');
+Route::post('addfooter','HomeController@Footeradd');
+
+Route::any('About','AboutController@About');
+Route::post('addabout','AboutController@Aboutadd');
+
+Route::any('Slider','HomeController@Slider');
+Route::post('addslider','HomeController@Slideradd');
+
+Route::any('Brand','BrandController@Brand');
+Route::post('addbrand','BrandController@Brandadd');
+
+Route::any('Ourteam','OurteamController@Ourteam');
+Route::post('addourteam','OurteamController@Ourteamadd');
+
+Route::any('Pressrelease','PressreleaseController@Pressrelease');
+Route::post('addpressrelease','PressreleaseController@Pressreleaseadd');
+
+Route::any('Pressreleaseinfo','PressreleaseController@Pressreleaseinfo');
+Route::post('addpressreleaseinfo','PressreleaseController@Pressreleaseinfoadd');
